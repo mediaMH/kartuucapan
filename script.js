ba2ac0934206909617c071c84625ebb0d2e5bec0
mediaMH <mhmedia2005@gmail.com>
2025-03-14 06:56:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var audio = document.getElementById("background-audio");
+var audio = document.getElementById("bg-audio");
 audio.volume = 1.0; // Pastikan volume maksimal
 audio.muted = false; // Pastikan tidak mute
